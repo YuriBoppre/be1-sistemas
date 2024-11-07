2029d6da7d4996ccfd9b3e151ec73133be79834e
YuriBoppre <yuriboppre@gmail.com>
2024-11-07 16:53:10 -0300

    Validations

--- src/modules/access/access.controller.ts
+++ src/modules/access/access.controller.ts
@@ -1,15 +1,37 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, InternalServerErrorException, NotFoundException, Param, Query } from '@nestjs/common';
 import { AccessService } from './access.service';
 
 @Controller('modulo')
 export class AccessController {
   constructor(private readonly accessService: AccessService) {}
 
-  @Get('cadastro/:dbName/fornecedores')
-  async getDataFromModule(@Param('dbName') dbName: string) {
+  @Get(':module/fornecedores')
+  async getDataFornecedores(@Param('module') module: string, @Query() filters: Record<string, any>) {
     //IDTipo = 8 para ser fornecedores (ADM)
-    const query = 'SELECT Nome FROM Cadastros WHERE [ID Tipo] = 8 ORDER BY Nome ASC';
+    const query = 'SELECT [ID Cadastro] as IdFornecedor, Nome, UF FROM Cadastros WHERE [ID Tipo] = 8 ORDER BY Nome ASC';
     
-    return await this.accessService.getData(query, 'cadastro', dbName);
+    try {
+      return await this.accessService.getDataWithFilters(query, module, filters);
+    } catch (er) {
+      if (er.message.includes('Módulo não encontrado!'))
+        throw new NotFoundException(er.message)
+
+      throw new InternalServerErrorException('Erro ao buscar dados dos fornecedores')
+    }    
+  }
+
+  @Get(':module/clientes')
+  async getDataClientes(@Param('module') module: string, @Query() filters: Record<string, any>) {
+    //IDTipo = 2 para ser clientes
+    const query = 'SELECT [ID Cadastro] as IdCliente, Nome, UF FROM Cadastros WHERE [ID Tipo] = 2 ORDER BY Nome ASC';
+    
+    try {
+      return await this.accessService.getDataWithFilters(query, module, filters);
+    } catch (er) {
+      if (er.message.includes('Módulo não encontrado!'))
+        throw new NotFoundException(er.message)
+
+      throw new InternalServerErrorException('Erro ao buscar dados dos clientes')
+    }    
   }
 }
--- src/services/database-conection.service.ts
+++ src/services/database-conection.service.ts
@@ -1,28 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, InternalServerErrorException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import * as ADODB from 'node-adodb';
 
 @Injectable()
 export class DatabaseConnectionService {
   private connection;
+  private readonly logger = new Logger(DatabaseConnectionService.name);
+
+  constructor(private readonly configService: ConfigService) {}
 
   async connectToDatabase(dbPath: string): Promise<any> {
-
-    const connectionString = `Provider=Microsoft.Jet.OLEDB.4.0;Data Source="${dbPath}";`;
-
-    try {
-      this.connection = ADODB.open(connectionString);
-      console.log(`Conectado ao banco de dados Access em: ${dbPath}`);
-      return this.connection;
-    } catch (error) {
-      console.error('Erro ao conectar ao banco Access:', error);
-      throw new Error(`Erro ao conectar ao banco Access: ${error.message}`);
+    const provider = this.configService.get<string>('ACCESS_PROVIDER');
+    
+    if (!provider) {
+      this.logger.error('Provider para conexão com Access não configurado');
+      throw new InternalServerErrorException('Provider não configurado');
     }
+
+    const connectionString = `Provider=${provider};Data Source="${dbPath}";`;
+
+    if (!this.connection) {
+      try {
+        this.connection = ADODB.open(connectionString);
+        this.logger.log(`Conectado ao banco de dados Access em: ${dbPath}`);
+      } catch (er) {
+        this.logger.error('Erro ao conectar ao banco Access:', er.message);
+        throw new InternalServerErrorException(`Erro ao conectar ao banco Access: ${er.message}`);
+      }
+    }
+
+    return this.connection;
   }
 
-  async closeConnection() {
+  async closeConnection() : Promise<void> {
     if (this.connection) {
-      await this.connection.close();
-      console.log('Conexão com o banco de dados Access encerrada.');
+      try {
+        // await this.connection.close();
+        this.connection = null;
+        this.logger.log('Conexão com o banco de dados Access encerrada.');
+      } catch (er) {
+        this.logger.error('Erro ao fechar a conexão:', er.message);
+        throw new InternalServerErrorException('Erro ao fechar a conexão');
+      }
     }
   }
 }
